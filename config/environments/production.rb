02c6155fc8a985848b8e5b5a68e24cf8490d213a
HyunSeo Nam <auspice7@naver.com>
2016-12-06 07:43:25 +0000

    precompile

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -22,14 +22,15 @@ Rails.application.configure do
 
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
-  config.serve_static_files = ENV['RAILS_SERVE_STATIC_FILES'].present?
+  # config.serve_static_files = ENV['RAILS_SERVE_STATIC_FILES'].present?
+  config.serve_static_files = true  #precompile 할 때 수정한 부분. 위의 주석에서 이 줄로
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true  #precompile 할 때 수정한 부분. false => true
 
   # Asset digests allow you to set far-future HTTP expiration dates on all assets,
   # yet still be able to expire them through the digest params.
--- db/migrate/20161201073818_create_pastwheathers.rb
+++ db/migrate/20161201073818_create_pastwheathers.rb
@@ -2,6 +2,9 @@ class CreatePastwheathers < ActiveRecord::Migration
   def change
     create_table :pastwheathers do |t|
       t.float  :temperature
+      t.int :x_point
+      t.int :y_point
+      
       t.timestamps null: false
     end
   end
